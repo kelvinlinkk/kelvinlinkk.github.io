e8ea978dc94095c9c929140bdbb42a84c982be30
Kun-Yi Lin <kelvinlinkk@outlook.com>
2022-08-21 16:56:01 +0800

    Sun Aug 21 16:50:08 2022auto update

--- autogit.py
+++ autogit.py
@@ -10,7 +10,7 @@ if flag:
     while(1):
         os.system("git add .")
         if len(commitMes)==0:
-            commitMes = time.ctime() + "時auto update"
+            commitMes = time.ctime() + "時 auto update"
         os.system("git commit -m \"" + commitMes +"\"")
         os.system("git push -u origin main")
         time.sleep(timer)
