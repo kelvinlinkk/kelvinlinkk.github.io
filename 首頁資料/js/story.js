185ff4037e0e711f5394826b0450d22038786a7d
Kun-Yi Lin <kelvinlinkk@outlook.com>
2023-05-29 06:13:17 +0800

    電腦自動同步:update

--- 首頁資料/js/story.js
+++ 首頁資料/js/story.js
@@ -32,12 +32,32 @@ function pose(ele){
     window.scrollBy({top:ele.getBoundingClientRect().top, behavior:"smooth"})
 };
 
-function set(t){
-    var main = document.getElementById("main");
+function myMove(elem) {
+    var elem = document.getElementById(elem); 
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+      if (pos == 350) {
+        clearInterval(id);
+      } else {
+        pos++; 
+        
+      }
+    }
+}
+
+function set(t, IDname){
+    var section = document.getElementById(IDname);
+    var i = 0;
     if(t == 0){
-        main.style.left = "50px"
+        section.style.visibility = "hidden";
+        for(i=0; i<300;i++){
+            section.style.width = i + 'px' ;
+        }
     }
     if(t == 1){
-        main.style.left = "150px";
+        section.style.visibility = "visible";
+        myMove(IDname);
     }
 }
