95c1859dc63e2684819e460f500ae03d5d20f76c
Kun-Yi Lin <kelvinlinkk@outlook.com>
2023-07-21 11:30:54 +0800

    back to old ones

--- 首頁資料/js/home.js
+++ 首頁資料/js/home.js
@@ -1,43 +1,38 @@
+var flag = false;
+var mouse = "";
+var prevmouse = " ";
 //info is from href.js
-function drawLogo() {
+function drawLogo(){
         for (let j = 0; j < info.length; j++) {
-                document.write('<article><h1 onclick=\"showthis(\'list' + j + '\')\">' + infoname[j] + '</h1><section id=\"list' + j + '\">');
-                for (let i = 0; i < info[j].length; i++) {
-                        document.write('<div class="infos"><a href="' + info[j][i][0] + '"><span>' + info[j][i][1] + '</span><img src="https://github.com/kelvinlinkk/web-source/raw/main/網頁資料/icon/' + info[j][i][2] + '" onmouseenter="inMouse(' + j + ',' + i + ')" onmouseout="mouse=\'\';document.getElementById(\'mousebar\').style.opacity = \'0\';"></a></div>')
-                }
+                document.write('<article><h1>'+ infoname[j] +'</h1><section>');
+                for(let i =0; i<info[j].length;i++){
+                        document.write('<div class="infos"><a href="' + info[j][i][0] +'"><span>'+ info[j][i][1] + '</span><img src="https://github.com/kelvinlinkk/web-source/raw/main/網頁資料/icon/'+ info[j][i][2] +'" onmouseenter="inMouse('+ j + ',' + i +')" onmouseout="mouse=\'\';document.getElementById(\'mousebar\').style.opacity = \'0\';"></a></div>')}   
                 document.write('</section></article>');
         }
 };
 
-function fade(name, speed, io) {
-        for (let i = 0; i < 100; i++) {
-                setTimeout(function () {
-                        if (io == 1) { document.getElementById(name).style.opacity = String(i / 100); }
-                        else { document.getElementById(name).style.opacity = String((100 - i) / 100); }
-                }, speed * i)
+
+function display(){ 
+        var search = document.getElementById("search");
+        var time = document.getElementById("time");
+        var countdown = document.getElementById("countdown");
+        if(flag){flag = !flag;
+                for (let i = 0;i<100;i++) {
+                        setTimeout(function(){
+                        search.style.opacity = String(i/100);
+                        time.style.opacity = String(i/100);
+                        countdown.style.opacity = String(i/100);},10*i)
+                }
+        }     
+        else{flag = !flag;
+                for (let i = 100;i>0;i--) {
+                        setTimeout(function(){
+                        search.style.opacity = String(i/100);
+                        time.style.opacity = String(i/100);
+                        countdown.style.opacity = String(i/100);},10*(100-i))
+                }
         }
 }
-
-function start() {
-        for (let i = 0; i < 100; i++) {
-                fade('cover', 10, 0);
-                fade('main', 10, 1);
-                fade('search', 10, 1);
-                fade('page', 10, 1);
-        }
-        setTimeout(function () {
-                document.getElementById('cover').style.display = "none";
-        }, 1000);
-}
-
-function showthis(elem) {
-        document.getElementById(elem).style.display = 'inherit';
-        fade(elem, 5, 1);
-}
-function closeall() {
-        for (let j = 0; j < info.length; j++) {
-                fade('list' + j, 5, 0);
-                setTimeout(() => document.getElementById('list' + j).style.display = 'none', 500);
-        }
-
+function Fade(){
+        for (let i = 0;i<100;i++) {setTimeout(function(){clearTimeout();document.getElementById("mousebar").style.opacity = String(i/100);},i*2)}
 }
