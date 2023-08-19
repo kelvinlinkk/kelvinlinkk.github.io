a58de491bfe641c9888d09c2108ca5e9de74a159
Kun-Yi Lin <108348635+kelvinlinkk@users.noreply.github.com>
2023-08-19 10:20:15 +0800

    avoid negative number in countdown.js

--- 首頁資料/js/countdown.js
+++ 首頁資料/js/countdown.js
@@ -14,11 +14,11 @@ var countd = function(date_string) {
  
     var d = Math.round((h - 12) / 24);
     h = Math.round(h % 24);
- 
-    document.getElementById("days").innerHTML = d;
-    document.getElementById("hours").innerHTML = (h < 10) ? "0" + h : h;
-    document.getElementById("minutes").innerHTML = (m < 10) ? "0" + m : m;
-    document.getElementById("seconds").innerHTML = (s < 10) ? "0" + s : s;
+
+    document.getElementById("days").innerHTML = Math.max(d,0);
+    document.getElementById("hours").innerHTML = (h < 10) ? "0" + Math.max(h,0) : h;
+    document.getElementById("minutes").innerHTML = (m < 10) ? "0" + Math.max(m,0) : m;
+    document.getElementById("seconds").innerHTML = (s < 10) ? "0" + Math.max(s,0) : s;
     document.getElementById("end").innerHTML = end.toString().split(' ').splice(1, 4).join(' ');
 };
 var setdate = function(date_string){
