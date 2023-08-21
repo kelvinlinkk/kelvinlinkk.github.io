8a04f40fc3121050fb9ad1bb0c0eae0974d4dfc3
Kun-Yi Lin <108348635+kelvinlinkk@users.noreply.github.com>
2023-08-21 22:48:04 +0800

    home -> index

---
+++ 首頁資料/js/index.js
@@ -0,0 +1,46 @@
+addEventListener('scroll',starting,false)
+function Bluring(obj,speed,depth,frames,f){
+        if (f){for (let i = 0;i<frames;i++) {setTimeout(function(){clearTimeout();obj.style.filter = "blur(" + String(i*depth/frames) + "px)";},i*speed)}}
+        else{for (let i = 0;i<frames;i++) {setTimeout(function(){clearTimeout();obj.style.filter = "blur(" + String((frames-i)*depth/frames) + "px)";},i*speed)}} 
+}
+function Fading(obj,speed,frames,f){
+        if (f){for (let i = 0;i<frames;i++) {setTimeout(function(){clearTimeout();obj.style.opacity = String(i/frames);},i*speed)}}
+        else{for (let i = 0;i<frames;i++) {setTimeout(function(){clearTimeout();obj.style.opacity = String((frames-i)/frames);},i*speed)}} 
+}
+//info is from href.js
+function drawLogo(){
+        for (let j = 0; j < info.length; j++) {
+                document.write('<article><h1>'+ infoname[j] +'</h1><section>');
+                for(let i =0; i<info[j].length;i++){
+                        document.write('<div class="infos"><a href="' + info[j][i][0] +'"><span>'+ info[j][i][1] + '</span><img src="首頁資料/logo/'+ info[j][i][2] +'"></a></div>')}   
+                document.write('</section></article>');
+        }
+};
+function starting() {
+        main=document.getElementById('main')
+        myvid=document.getElementById('myvid')
+        if (document.getElementsByTagName("body")[0].getBoundingClientRect().top >= 0-screen.height/5) {
+                if(document.getElementById("main").style.opacity == 0.99)
+                {Fading(main, 5,100, 0);Bluring(myvid, 20, 5, 30, 0)}
+        }
+        else if(document.getElementById("main").style.opacity <= 0.01) {
+                Fading(main, 5,100, 1);Bluring(myvid, 20, 5, 30, 1)
+        }
+}
+function display() {
+        var welcome = document.getElementsByClassName('welcome')[0].getElementsByTagName('h1')[0]
+        var articles = document.getElementsByTagName('article')
+        setTimeout(function(){Fading(welcome,12,60,1)},1500)
+        for(i = 0;i<articles.length;i++){
+                articles[i].getElementsByTagName('h1')[0].addEventListener('click',function()
+        {
+                let mysection = this.parentNode.getElementsByTagName('section')[0]
+                let myflag = (mysection.style.display != 'block')
+                if (myflag == 1) {
+                        setTimeout(function () { mysection.style.setProperty('display', 'block', 'important'); }, 1)
+                        Fading(mysection, 5,100, myflag);}
+                else if (mysection.style.opacity == '0.99') {
+                        setTimeout(function () { mysection.style.setProperty('display', 'none', 'important'); }, 800)
+                        Fading(mysection, 5,100, myflag);}
+        },false)}            
+}
