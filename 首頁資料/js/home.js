ee97aec9ec02e12525eb056c2bd70217b4ebedd7
Kun-Yi Lin <108348635+kelvinlinkk@users.noreply.github.com>
2023-08-21 09:23:28 +0800

    電腦自動同步:update

--- 首頁資料/js/home.js
+++ 首頁資料/js/home.js
@@ -1,66 +1,46 @@
 var flag = false;
-var flagArr = [];
-addEventListener('scroll',starting,false)
-
-function Fading(id,speed,f){
-        if (f){for (let i = 0;i<100;i++) {setTimeout(function(){clearTimeout();document.getElementById(id).style.opacity = String(i/100);},i*speed)}}
-        else{for (let i = 0;i<100;i++) {setTimeout(function(){clearTimeout();document.getElementById(id).style.opacity = String((100-i)/100);},i*speed)}} 
+addEventListener('mouseover',starting,false)
+function Bluring(obj,speed,depth,frames,f){
+        if (f){for (let i = 0;i<frames;i++) {setTimeout(function(){clearTimeout();obj.style.filter = "blur(" + String(i*depth/frames) + "px)";},i*speed)}}
+        else{for (let i = 0;i<frames;i++) {setTimeout(function(){clearTimeout();obj.style.filter = "blur(" + String((frames-i)*depth/frames) + "px)";},i*speed)}} 
 }
-function Bluring(id,speed,depth,frames,f){
-        if (f){for (let i = 0;i<frames;i++) {setTimeout(function(){clearTimeout();document.getElementById(id).style.filter = "blur(" + String(i*depth/frames) + "px)";},i*speed)}}
-        else{for (let i = 0;i<frames;i++) {setTimeout(function(){clearTimeout();document.getElementById(id).style.filter = "blur(" + String((frames-i)*depth/frames) + "px)";},i*speed)}} 
-}
-function FadingByObj(obj,speed,f){
+function Fading(obj,speed,f){
         if (f){for (let i = 0;i<100;i++) {setTimeout(function(){clearTimeout();obj.style.opacity = String(i/100);},i*speed)}}
         else{for (let i = 0;i<100;i++) {setTimeout(function(){clearTimeout();obj.style.opacity = String((100-i)/100);},i*speed)}} 
 }
 //info is from href.js
-function drawLogo(num){//這部分要改善 就是要找出第一個drawlogo
+function drawLogo(){
         for (let j = 0; j < info.length; j++) {
-                document.write('<article><h1 onclick="displaySec(' + (j+num) + ')">'+ infoname[j] +'</h1><section>');
+                document.write('<article><h1>'+ infoname[j] +'</h1><section>');
                 for(let i =0; i<info[j].length;i++){
                         document.write('<div class="infos"><a href="' + info[j][i][0] +'"><span>'+ info[j][i][1] + '</span><img src="首頁資料/logo/'+ info[j][i][2] +'"></a></div>')}   
                 document.write('</section></article>');
-                flagArr[j]=0
         }
-};  
-function display(){ 
-        var search = document.getElementById("search");
-        var time = document.getElementById("time");
-        var countdown = document.getElementById("countdown");
-        if(flag){
-                flag = !flag;
-                for (let i = 0;i<100;i++) {
-                        setTimeout(function(){
-                        search.style.opacity = String(i/100);
-                        time.style.opacity = String(i/100);
-                        countdown.style.opacity = String(i/100);},10*i)
-                }
-        }     
-        else{
-                flag = !flag;
-                for (let i = 100;i>0;i--) {
-                        setTimeout(function(){
-                        search.style.opacity = String(i/100);
-                        time.style.opacity = String(i/100);
-                        countdown.style.opacity = String(i/100);},10*(100-i))
-                }
+};
+function starting() {
+        schedule=document.getElementById('schedule')
+        main=document.getElementById('main')
+        myvid=document.getElementById('myvid')
+        if (document.getElementsByTagName("body")[0].getBoundingClientRect().top == "0" ) {
+                Fading(schedule, 5, 0);Fading(main, 5, 0);Bluring(myvid, 80, 5, 20, 0)
+        }
+        
+        else if (document.getElementById("main").style.opacity <= 0.99) {
+                Fading(schedule, 5, 1);Fading(main, 5, 1);Bluring(myvid, 80, 5, 20, 1)
         }
 }
-function starting(){
-        if(document.getElementsByTagName("body")[0].getBoundingClientRect().top == "0"){
-        Fading("schedule",5,0)
-        Fading("main",5,0)
-        Bluring("myvid",80,5,20,0)}
-        if(document.getElementById("main").style.opacity <= 0.01){
-        Fading("schedule",5,1)
-        Fading("main",5,1)
-        Bluring("myvid",80,5,20,1)
-}}
-function displaySec(num){
-        let mysection = document.getElementsByTagName("article")[num].getElementsByTagName('section')[0]
-        flagArr[num] = !flagArr[num]
-        FadingByObj(mysection,5,flagArr[num]);
-        if(flagArr[num]==1){setTimeout(function(){mysection.style.setProperty('display','block','important');},1)}
-        else{setTimeout(function(){mysection.style.setProperty('display','none','important');},800)}
+function display() {
+        var articles = document.getElementsByTagName('article')
+        for(i = 0;i<articles.length;i++){
+                articles[i].getElementsByTagName('h1')[0].addEventListener('click',function()
+        {
+                let mysection = this.parentNode.getElementsByTagName('section')[0]
+                let myflag = (mysection.style.display != 'block')
+                if (myflag == 1) {
+                        setTimeout(function () { mysection.style.setProperty('display', 'block', 'important'); }, 1)
+                        Fading(mysection, 5, myflag);}
+                else if (mysection.style.opacity == '0.99') {
+                        setTimeout(function () { mysection.style.setProperty('display', 'none', 'important'); }, 800)
+                        Fading(mysection, 5, myflag);}
+        },false)}            
 }
