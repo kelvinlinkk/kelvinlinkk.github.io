4fb975d8db5bad423264c340b3b242d99cae9f9d
Kun-Yi Lin <108348635+kelvinlinkk@users.noreply.github.com>
2023-08-23 14:39:41 +0800

    電腦自動同步:update

--- 首頁資料/js/index.js
+++ 首頁資料/js/index.js
@@ -1,3 +1,4 @@
+blurflag = 2
 addEventListener('scroll',starting,false)
 function Bluring(obj,speed,depth,frames,f){
         if (f){for (let i = 0;i<frames;i++) {setTimeout(function(){clearTimeout();obj.style.filter = "blur(" + String(i*depth/frames) + "px)";},i*speed)}}
@@ -17,15 +18,19 @@ function drawLogo(){
         }
 };
 function starting() {
-        main=document.getElementById('main')
         myvid=document.getElementById('myvid')
-        if (document.getElementsByTagName("body")[0].getBoundingClientRect().top >= 0-screen.height/5) {
-                if(document.getElementById("main").style.opacity == 0.99)
-                {Fading(main, 5,100, 0);Bluring(myvid, 20, 5, 30, 0)}
+        if (document.getElementsByTagName("body")[0].getBoundingClientRect().top < 0-screen.height/5) {
+                if(blurflag==2){
+                        blurflag=1;
+                        setTimeout(function(){blurflag=0;},200)
+                        Bluring(myvid, 20, 5, 30, 1);
+                }
         }
-        else if(document.getElementById("main").style.opacity <= 0.01) {
-                Fading(main, 5,100, 1);Bluring(myvid, 20, 5, 30, 1)
-        }
+        else if(blurflag==0){
+                blurflag=1;
+                setTimeout(function(){blurflag=2;},200)
+                Bluring(myvid, 20, 5, 30, 0);
+        } 
 }
 function display() {
         var articles = document.getElementsByTagName('article')
