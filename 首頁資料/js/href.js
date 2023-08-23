7c9ffa3905087a3cc4168cf32cd471265751a18c
Kun-Yi Lin <108348635+kelvinlinkk@users.noreply.github.com>
2023-08-23 20:21:23 +0800

    從電腦autosave自動同步

--- 首頁資料/js/href.js
+++ 首頁資料/js/href.js
@@ -66,7 +66,7 @@ var info = [
     ["https://www.tiktok.com/", "tiktok", "tiktok.jpg"],
     ["https://www.formula1.com/", "f1", "f1.svg"],
     ["https://www.motogp.com/", "motogp", "motogp.png"],
-    ["https://www.motorsport.com/", "motorsport", "motorsport.jpg"],
+    ["https://www.motorsport.com/", "motorsport", "motorsport.png"],
     ["https://www.autosport.com/", "autosport", "autosport.jpg"],
     ["https://www.sportsv.net/racing", "運動視界", "運動視界.jpg"]],
     /*程式*/
