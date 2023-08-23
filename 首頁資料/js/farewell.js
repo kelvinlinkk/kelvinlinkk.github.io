fae608e7b6e0a3695e96d55381990965834060c6
Kun-Yi Lin <108348635+kelvinlinkk@users.noreply.github.com>
2023-08-23 09:33:53 +0800

    電腦自動同步:update

--- 首頁資料/js/farewell.js
+++ 首頁資料/js/farewell.js
@@ -1,14 +1,14 @@
 const flag = []
 const flag2 = []
-window.onload = function(){
-    for(let i = 0; i < document.getElementsByTagName("article").length; i++){
-        addEventListener('scroll', function(){track(i)}, false);
+window.onload = function () {
+    for (let i = 0; i < document.getElementsByTagName("article").length; i++) {
+        addEventListener('scroll', function () { track(i) }, false);
         flag.push(false);
         flag2.push(false);
     }
     track(0);
 }
-function track(n){
+function track(n) {
     var tar = document.getElementsByTagName("article")[n];
     var h1 = tar.getElementsByTagName('h1')[0];
     var lyrics = tar.getElementsByTagName('p')[0];
@@ -19,49 +19,49 @@ function track(n){
     tarBot = tar.getBoundingClientRect().bottom;
     console.log(flag2 + ' ' + tarBot + ' ' + contentTop + ' ' + screen.height);
     //LYRICS
-    if(tarTop > screen.height || tarBot < 0){
+    if (tarTop > screen.height || tarBot < 0) {
         lyrics.style.opacity = "0";
         flag[n] = false;
-    }else if(flag[n] == false){
-        fade(lyrics,3);
+    } else if (flag[n] == false) {
+        fade(lyrics, 3);
         flag[n] = true;
     }
     //H1
-    if(tarBot < 0 || contentTop > screen.height -200){
+    if (tarBot < 0 || contentTop > screen.height - 200) {
         mypic.style.opacity = '0';
         h1.style.left = '-10%';
         mytext.style.transform = 'scale(0.9,0.9)';
         flag2[n] = false;
     }
-    else if(tarBot >= 0 && flag2[n] == false){
+    else if (tarBot >= 0 && flag2[n] == false) {
         move(h1);
-        fade(mypic,2);
-        bounce(mytext,1.1);
+        fade(mypic, 2);
+        bounce(mytext, 1.1);
         flag2[n] = true;
-    }     
+    }
 }
 
-function move(tar){
-    for (let i = 0;i<1000;i++){
+function move(tar) {
+    for (let i = 0; i < 1000; i++) {
         setTimeout(
-            function(){
-                clearTimeout(tar);tar.style.left = String((i-2000)/200) + '%';
-            },i*1.5)
+            function () {
+                clearTimeout(tar); tar.style.left = String((i - 2000) / 200) + '%';
+            }, i * 1.5)
     }
 }
-function fade(tar, speed){
-    for (let i = 0;i<1000;i++){
+function fade(tar, speed) {
+    for (let i = 0; i < 1000; i++) {
         setTimeout(
-            function(){
-                clearTimeout();tar.style.opacity = i/1000;
-            },i*speed)
+            function () {
+                clearTimeout(); tar.style.opacity = i / 1000;
+            }, i * speed)
     }
 }
-function bounce(tar, speed){
-    for (let i = 0;i<1000;i++){
+function bounce(tar, speed) {
+    for (let i = 0; i < 1000; i++) {
         setTimeout(
-            function(){
-                clearTimeout();tar.style.transform = 'scale(' + (9000+i)/10000 + ',' + (9000 + i)/10000 + ')' ;
-            },i*speed)
+            function () {
+                clearTimeout(); tar.style.transform = 'scale(' + (9000 + i) / 10000 + ',' + (9000 + i) / 10000 + ')';
+            }, i * speed)
     }
 }
