91f657a031d45a8d06239dc7719652e20ee89093
Kun-Yi Lin <108348635+kelvinlinkk@users.noreply.github.com>
2023-08-23 18:37:37 +0800

    從電腦autosave自動同步

---
+++ 首頁資料/motogp.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.autosport.com/motogp/standings/2023/"
+soup = BeautifulSoup(requests.get(url).text,'html.parser')
+datas = []
+count = 1
+for point,ridername in zip(soup.find_all(class_='ms-table_cell ms-table_field--total_points'),
+                                    soup.find_all(class_="name")
+                                ):
+    datas.append([count,
+                  point.getText(),
+                  ridername.getText().strip()])
+    count+=1
+with open('motogp.html','w',encoding='utf-8') as f:
+    f.write("""
+<!DOCTYPE html>
+<html>
+<head>
+  <title>車手排行</title>
+  <link href="首頁資料/css/motogp.css" rel="stylesheet" type="text/css">
+  <script src="首頁資料/js/f1.js"></script>
+<head>
+<body>
+<h1>STANDINGS</h1>
+    <hr>
+  <table>
+    <tr>
+      <th>車手排名</th>
+      <th>車手名字</th>
+      <th>積分</th>
+    </tr>
+""")
+    for info in datas:
+        f.write(f"<tr><td>{info[0]}</td><td>{info[2]}</td><td>{info[1]}</td></tr>")
+    
+print(datas)
