0d41dd64ce54cdf9c44c081d8c9846d4e9924932
Kun-Yi Lin <108348635+kelvinlinkk@users.noreply.github.com>
2023-09-08 22:18:53 +0800

    從電腦autosave自動同步

--- 首頁資料/js/index.js
+++ 首頁資料/js/index.js
@@ -1,5 +1,4 @@
 myshuffle = 4
-themelist=["#3c3834","#040606","#11140e","#3f2a28"]
 function Fading(obj,speed,frames,f){
         if (f){for (let i = 0;i<=frames;i++) {setTimeout(function(){clearTimeout();obj.style.opacity = String(i/frames);},i*speed)}}
         else{for (let i = 0;i<=frames;i++) {setTimeout(function(){clearTimeout();obj.style.opacity = String((frames-i)/frames);},i*speed)}} 
@@ -32,20 +31,11 @@ function display() {
 
 function shuffling() {
         myshuffle = (myshuffle+1)%4
-        myvid = document.getElementsByTagName('video')
         body = document.getElementsByTagName('body')[0]
         mask = document.getElementsByClassName('mask')[0]
         h1 = document.getElementsByTagName('h1')
         for(i=0;i<4;i++){
-            if(i!=myshuffle){
-                myvid[i].style.display = "none";h1[i].style.display = "none";
-            }else{
-                myvid[i].style.display = "block";
-                Fading(myvid[i], 5,50, 1);
-                h1[i].style.display = "block";
-            }    
-        }
-        if(body.getBoundingClientRect().top==0){
-                body.style.backgroundColor = themelist[myshuffle]
-                mask.style.background = "linear-gradient(#00000000 77%," + themelist[myshuffle] + "b4 90%," + themelist[myshuffle] + " 100%)"}                   
+            if(i!=myshuffle){h1[i].style.display = "none";}
+            else{h1[i].style.display = "block";}    
+        }                   
 }
