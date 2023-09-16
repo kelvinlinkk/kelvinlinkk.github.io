5c8403a8febe85eefb4ba97a4128550a672332d2
Kun-Yi Lin <108348635+kelvinlinkk@users.noreply.github.com>
2023-09-16 21:00:23 +0800

    從電腦autosave自動同步

--- 首頁資料/js/rotate.js
+++ 首頁資料/js/rotate.js
@@ -1,7 +1,9 @@
 ballx=[30,60,90,120]
 bally=[0,0,0,0]
 ballr=[20,20,20,20]
-degree=0
+angle=0
+acceleration = -1000
+speed = 500
 function setPos(obj,x,y,r){
     obj.style.width=String(r) + 'px'
     obj.style.height=String(r) + 'px'
@@ -11,15 +13,18 @@ function setPos(obj,x,y,r){
 for(let i=1;i<=4;i++){
     setInterval(()=>{setPos(document.getElementById('ball'+String(i)), ballx[i-1],bally[i-1],ballr[i-1])},1)
 }
-function rotate(num,degree){
+function rotate(num,a){
+    myangle = a/180*Math.PI
     let x = ballx[num-1]
     let y = bally[num-1]
-    ballx[num-1] = x*Math.cos(degree) - y*Math.sin(degree);
-    bally[num-1] = x*Math.sin(degree) + y*Math.cos(degree);
+    ballx[num-1] = x*Math.cos(myangle) - y*Math.sin(myangle);
+    bally[num-1] = x*Math.sin(myangle) + y*Math.cos(myangle);
 }
 function move(num,x,y){
     ballx[num-1] += x;
     bally[num-1] += y;
 }
-setInterval(()=>{rotate(4,degree);},0)
-document.getElementById('degree').addEventListener('change',(e)=>{degree=e.target.value},false)
+setInterval(()=>{rotate(4,angle/200);},5)
+document.getElementById('angle').addEventListener('change',(e)=>{angle=e.target.value},false)
+
+setInterval(()=>{move(3,1,speed/100);speed+=acceleration*0.01},10)
