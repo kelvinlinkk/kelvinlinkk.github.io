e0e050ae6f18d7285d42db2e800722bfbae9d1f5
Kun-Yi Lin <108348635+kelvinlinkk@users.noreply.github.com>
2023-11-18 19:39:49 +0800

    從電腦autosave自動同步

--- 首頁資料/js/card.js
+++ 首頁資料/js/card.js
@@ -1,2 +1,10 @@
 const myadd = document.getElementById("plus")
-myadd.addEventListener("click",function(){console.log(myadd.parentNode.childNodes[1].innerHTML=parseInt(myadd.parentNode.childNodes[1].innerHTML)+1)})
+const stone = document.getElementById("stone-num")
+const pools = document.getElementsByClassName("pool")
+var poolnames=["卡池一","卡池二","卡池三","卡池四",]
+const pool = document.getElementsByClassName("activity-title")[0]
+pool.innerHTML = poolnames[0]
+myadd.addEventListener("click",function(){console.log(stone.value=parseInt(stone.value)+1)})
+for(let i = 0; i < pools.length;i++){
+    pools[i].addEventListener('click',()=>{pool.innerHTML = poolnames[i]})
+}
