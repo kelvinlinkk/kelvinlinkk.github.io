8b156f1241dd22fbb7d7735d78a0717d847573d7
Kun-Yi Lin <108348635+kelvinlinkk@users.noreply.github.com>
2023-11-30 19:05:05 +0800

    從電腦autosave自動同步

--- 首頁資料/js/card.js
+++ 首頁資料/js/card.js
@@ -2,10 +2,14 @@ const myadd = document.getElementById("plus")
 const stone = document.getElementById("stone-num")
 const pools = document.getElementsByClassName("pool")
 const genshin = document.getElementById("genshin")
-var poolnames=["卡池一","卡池二","卡池三","卡池四",]
+const poolinfo = document.getElementById("poolinfo")
 const pool = document.getElementsByClassName("activity-title")[0]
+const gacha1 = document.getElementById("gacha-1")
+const gacha10 = document.getElementById("gacha-10")
+const transition = document.getElementById("transition")
+
+var poolnames=["卡池一","卡池二","卡池三","卡池四",]
 pool.innerHTML = poolnames[0]
-myadd.addEventListener("click",function(){console.log(stone.value=parseInt(stone.value)+1)})
 
 function fade(tar, speed) {
     for (let i = 0; i < 1000; i++) {
@@ -16,11 +20,17 @@ function fade(tar, speed) {
     }
 }
 
+function gacha(num){
+    transition.getElementsByTagName("video")[0].style.display="initial"
+}
+
 window.onload=function(){
     setTimeout(()=>{fade(genshin,1)},1000)
     setTimeout(()=>{genshin.style.display="none"},2000)
+    myadd.addEventListener("click",function(){console.log(stone.value=parseInt(stone.value)+1)})
+    gacha1.addEventListener("click",function(){gacha(1)})
 }
 
 for(let i = 0; i < pools.length;i++){
-    pools[i].addEventListener('click',()=>{pool.innerHTML = poolnames[i]})
+    pools[i].addEventListener('click',()=>{pool.innerHTML = poolnames[i];poolinfo.src = "首頁資料/卡池資訊/" + (i+1) + ".mp4";})
 }
