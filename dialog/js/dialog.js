280204d2a841d2a98316d4daf99f6fc74d63e768
Kun-Yi Lin <108348635+kelvinlinkk@users.noreply.github.com>
2025-01-23 21:34:57 +0800

    add intro

--- dialog/js/dialog.js
+++ dialog/js/dialog.js
@@ -1,39 +1,26 @@
+//constants
+const dialog = document.getElementById('dialog');
+const background = document.getElementById('bg');
+const startbutton = document.getElementById('startbutton')
+
 class DialogSystem {
     constructor() {
         this.linenum = 0;
-        this.islocked = false;
+        this.islocked = true;
+
+        //story here
         this.text = [
-            "Once upon a time, there was a small village nestled in the mountains.[img sunset cover]",
-            "The villagers lived peacefully,[n] tending to their gardens and livestock.[img 0 cover]"
+            "Once upon a time, there was a small village nestled in the mountains.[bg sunset cover]",
+            "The villagers lived peacefully,[n] tending to their gardens and livestock.[bg 0 cover]"
         ];
-        this.dialog = document.getElementById('dialog');
-        this.background = document.getElementById('bg');
-        
-        // 綁定點擊事件
-        document.addEventListener('click', () => {
+
+        this.showwords(this.linenum);
+        dialog.addEventListener('click', () => {
             this.islocked = true;
             this.showwords(this.linenum);
         });
     }
-
-    commandhandler(com) {
-        let params = com.split(" ");
-        switch(params[0]) {
-            case 'n':
-                return '<br>';
-            case 'img':
-                this.background.style.visibility = 'visible';
-                if(params[1] == 0) {
-                    this.background.style.visibility = 'hidden';
-                    break;
-                }
-                this.background.src = 'background/' + params[1] + '.jpg';
-                this.background.style.objectFit = params[2];
-                break;
-        }
-        return "";
-    }
-
+    // show words individually
     async showwords(num) {
         let words = this.text[num].split(""), 
             display = "", 
@@ -56,12 +43,52 @@ class DialogSystem {
             } 
             display += word;
             await new Promise(r => setTimeout(r, 10));
-            this.dialog.innerHTML = display;
+            dialog.innerHTML = display;
         }
         this.islocked = false;
         this.linenum += 1;
     }
+
+    //handle [] demmands
+    commandhandler(com) {
+        let params = com.split(" ");
+        switch(params[0]) {
+            case 'n':
+                //new line
+                return '<br>';
+            case 'bg':
+                //background src object-fit
+                if(params[1] == 0) {
+                    background.style.visibility = 'hidden';
+                } else {
+                    background.src = 'background/' + params[1] + '.jpg';
+                    background.style.visibility = 'visible';
+                    background.style.objectFit = params[2];
+                }
+                break;
+            case 'img':
+                //img display
+                break;
+            case 'audio':
+                //audio
+                break;
+            case 'effect':
+                //effect
+                break;
+        }
+        return "";
+    }
 }
 
-// 初始化對話系統
-const dialogSystem = new DialogSystem();
+//event listeners
+window.onload=()=>{
+    dialog.style.display = 'none';
+    background.src = "background/resize.png";
+}
+startbutton.onclick=()=>{
+    dialog.style.display = 'initial';
+    background.style.visibility = "hidden";
+    background.src = "";
+    startbutton.style.display = 'none';
+    const dialogSystem = new DialogSystem();
+}
